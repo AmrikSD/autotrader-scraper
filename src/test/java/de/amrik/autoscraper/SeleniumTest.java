c5560fde53a38e0e65cd9693da9b0d03f328b22e
AmrikSD <Amrik.singh@live.co.uk>
2020-11-18 15:48:21 +0000

    cc

--- src/test/java/de/amrik/autoscraper/SeleniumTest.java
+++ src/test/java/de/amrik/autoscraper/SeleniumTest.java
@@ -7,7 +7,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import static io.github.bonigarcia.wdm.WebDriverManager;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WebDriver;
