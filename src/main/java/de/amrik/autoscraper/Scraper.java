52a58a804438cfbc1ecd9bf96bbd8c64e088ca63
Amrik Singh <amrik.singh@live.co.uk>
2021-01-28 00:01:53 +0000

    Dev (#14)
    * typo
    
    * Fixed accept cookies issue
    
    * removed .swp
    
    * Should get all pages now
    
    * Removed scrolling, it's not actually needed, speeds up scraping
    
    * added fix for headless
    
    * scraping finally works, it's slow
    
    * Changed CI name
    
    * Removed empty test
    
    * IT GO BRR (multithreading done)
    
    * Update Browser.java
    
    * Added google formatting to keep things consistent,will look into forcing
    
    * xml formatting

--- src/main/java/de/amrik/autoscraper/Scraper.java
+++ src/main/java/de/amrik/autoscraper/Scraper.java
@@ -1,25 +1,17 @@
 package de.amrik.autoscraper;
 
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.ArrayList;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-/**
- * The main API interface.
- * From this you should be able to get everything you want.
- */
+/** The main API interface. From this you should be able to get everything you want. */
 class Scraper {
 
-  //Unknowns
+  // Unknowns, set to some random defaults :)
   String postcode = "SW1A2AA";
   String make = "";
   String model = "";
@@ -40,19 +32,14 @@ class Scraper {
 
   private static WebDriver driver;
 
+  public Scraper() {}
 
-  public Scraper(){
-  
-  }
-
-  /**
-   * Scraped data from autotrader.co.uk based on the parameters passed into the Scraper object
-   * */
-  public ArrayList <AutoAd> scrape(){
+  /** Scraped data from autotrader.co.uk based on the parameters passed into the Scraper object */
+  public ArrayList<AutoAd> scrape() {
 
     // Build the Query
     String url = AutoData.URL;
-    url += AutoData.SORT_BY + "relevance"; //Sort needs to go first.
+    url += AutoData.SORT_BY + "relevance"; // Sort needs to go first.
     url += AutoData.POSTCODE + this.postcode;
     url += AutoData.RADIUS + this.maxDistance;
     url += AutoData.TRANSMISSION + this.gearbox;
@@ -65,7 +52,6 @@ class Scraper {
 
     System.out.println(url);
 
-
     // Start Selenium
     WebDriverManager.chromedriver().setup();
     ChromeOptions options = new ChromeOptions();
@@ -76,29 +62,27 @@ class Scraper {
     driver = new ChromeDriver(options);
     driver.manage().window().maximize();
     driver.manage().timeouts().implicitlyWait(120, TimeUnit.MICROSECONDS);
-    
+
     // Add the cookie so that autotrader can stop being little bitches..
     driver.get(AutoData.URL);
     driver.manage().addCookie(AutoData.cookie);
 
-
     // Get total number of pages.
-    driver.get(url);   
-    WebElement pageCountElement = driver.findElement(AutoData.PAGES_CLASS); 
+    driver.get(url);
+    WebElement pageCountElement = driver.findElement(AutoData.PAGES_CLASS);
     String pagesStr = pageCountElement.getText().split(" ")[3];
     int pages = Integer.parseInt(pagesStr);
-    pages = Math.min(Math.min(pagesToSearch,pages),AutoData.MAX_PAGES);
+    pages = Math.min(Math.min(pagesToSearch, pages), AutoData.MAX_PAGES);
 
     // Close Selenium
-    if(driver != null){
+    if (driver != null) {
       driver.close();
     }
 
     // Call some browsers to get the actual Car objects....
     // TODO: MAKE IT IT GO BRRRR (MULTITHREADING)
-   
 
-    int noOfBrowsers = (int) Math.ceil(pages*1.0 / AutoData.MAX_PAGES_PER_DRIVER*1.0);
+    int noOfBrowsers = (int) Math.ceil(pages * 1.0 / AutoData.MAX_PAGES_PER_DRIVER * 1.0);
 
     Browser[] browsers = new Browser[noOfBrowsers];
     Thread[] threadList = new Thread[noOfBrowsers];
@@ -106,130 +90,130 @@ class Scraper {
 
     ArrayList<AutoAd> returnList = new ArrayList<AutoAd>();
 
-    for(int i = 0; i<noOfBrowsers; i++){
+    for (int i = 0; i < noOfBrowsers; i++) {
       int start = (i * AutoData.MAX_PAGES_PER_DRIVER);
-      int end =  Math.min(pages ,((i+1) * AutoData.MAX_PAGES_PER_DRIVER) -1);
+      int end = Math.min(pages, ((i + 1) * AutoData.MAX_PAGES_PER_DRIVER) - 1);
       cars[i] = new ArrayList<AutoAd>();
       browsers[i] = new Browser(url, start, end, cars[i]);
       threadList[i] = new Thread(browsers[i]);
     }
 
-
-    for(Thread t : threadList){
+    for (Thread t : threadList) {
       t.start();
     }
 
-    for(Thread t : threadList){
-      try{
+    for (Thread t : threadList) {
+      try {
         t.join();
-      }catch(Exception e){
+      } catch (Exception e) {
         System.out.println(e);
       }
     }
 
-    for(Browser b : browsers){
+    for (Browser b : browsers) {
       returnList.addAll(b.getAds());
     }
 
     return returnList;
   }
 
-
-  public void setPostcode(String postcode){
+  public void setPostcode(String postcode) {
     this.postcode = postcode;
   }
 
-  public void setMaxDistance(int maxDistance){
+  public void setMaxDistance(int maxDistance) {
     this.maxDistance = maxDistance;
   }
 
-  public void setMinPrice(int minPrice){
-   this.minPrice = minPrice;
+  public void setMinPrice(int minPrice) {
+    this.minPrice = minPrice;
   }
 
-  public void setMaxPrice(int maxPrice){
-   this.maxPrice = maxPrice;
+  public void setMaxPrice(int maxPrice) {
+    this.maxPrice = maxPrice;
   }
 
-  public void setMinYear(int minYear){
+  public void setMinYear(int minYear) {
     this.minYear = minYear;
   }
 
-  public void setMaxYear(int maxYear){
+  public void setMaxYear(int maxYear) {
     this.maxYear = maxYear;
   }
 
-  public void setGearbox(String gearbox){
+  public void setGearbox(String gearbox) {
     this.gearbox = gearbox;
   }
-  
-  public void setMinEngineSize(double engSize){
+
+  public void setMinEngineSize(double engSize) {
     this.minEngineSize = engSize;
   }
 
-  public void setMaxEngineSize(double engSize){
-    this.maxEngineSize = engSize; 
+  public void setMaxEngineSize(double engSize) {
+    this.maxEngineSize = engSize;
   }
 
-  public void setMake(String make){
+  public void setMake(String make) {
     this.make = make;
   }
 
-  public void setModel(String model){
-    this.model = model; 
+  public void setModel(String model) {
+    this.model = model;
   }
 
-  public void setPages(int pages){
-    this.pagesToSearch = Math.min(100,pages);
+  public void setPages(int pages) {
+    this.pagesToSearch = Math.min(100, pages);
   }
 
-  public String getPostcode(){
+  public String getPostcode() {
     return postcode;
   }
 
-  public int getMaxDistance(){
-   return maxDistance;
+  public int getMaxDistance() {
+    return maxDistance;
   }
 
-  public int getMinPrice(){
+  public int getMinPrice() {
     return minPrice;
   }
 
-  public int getMaxPrice(){
+  public int getMaxPrice() {
     return maxPrice;
   }
 
-  public int getMinYear(){
+  public int getMinYear() {
     return minYear;
   }
 
-  public int getMaxYear(){
+  public int getMaxYear() {
     return maxYear;
   }
 
-  public String getGearbox(){
+  public String getGearbox() {
     return gearbox;
   }
-  
-  public double getMaxEngineSize(){
+
+  public double getMaxEngineSize() {
     return maxEngineSize;
   }
 
-  public double getMinEngineSize(){
+  public double getMinEngineSize() {
     return minEngineSize;
   }
 
-  public String getMake(){
+  public String getMake() {
     return make;
   }
-  public String getModel(){
+
+  public String getModel() {
     return model;
   }
 
   /**
-   * return the number of pages the user wants to search, this can be more than what will actually be searched, if there are less pages on atotrader than the user wants..
-   * */
-  public int getPagesToSearch(){
+   * return the number of pages the user wants to search, this can be more than what will actually
+   * be searched, if there are less pages on atotrader than the user wants..
+   */
+  public int getPagesToSearch() {
     return pagesToSearch;
   }
 }
--- src/test/java/de/amrik/autoscraper/SeleniumTest.java
+++ src/test/java/de/amrik/autoscraper/SeleniumTest.java
@@ -1,62 +1,56 @@
 package de.amrik.autoscraper;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import java.util.concurrent.TimeUnit;
-
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-
-/**
- * Unit test for just selenium.
- *
- */
+/** Unit test for just selenium. */
 public class SeleniumTest {
 
-      private static WebDriver driver;
+  private static WebDriver driver;
 
-      @BeforeClass
-      public static void setUp()
-      {
-          WebDriverManager.chromedriver().setup();
-          ChromeOptions options = new ChromeOptions();
-          options.addArguments("--no-sandbox");
-          options.addArguments("--disable-dev-shm-usage");
-          options.addArguments("--headless");
-          driver = new ChromeDriver(options);
-          driver.navigate().to("https://www.google.com");
-          driver.manage().window().maximize();
-          driver.manage().timeouts().implicitlyWait(120, TimeUnit.MILLISECONDS);
-      }
-
-      @Test
-      public void getGoogle(){
-       System.out.println("Starting test " + new Object(){}.getClass().getEnclosingMethod().getName());
-
-       driver.get("https://www.google.com");
-       WebElement searchTxt = driver.findElement(By.name("q"));
-       WebElement submitBtn = driver.findElement(By.name("btnK"));
-
-       searchTxt.sendKeys("test");
-       submitBtn.click();
-
-       System.out.println("Current URL is: " + driver.getCurrentUrl());
-       Assert.assertTrue(driver.getTitle().contains("test"));
-       System.out.println("Current Title is: " + driver.getTitle());
-      }
-
-      @AfterClass
-      public static void tearDown(){
-          if (driver != null) {
-              driver.quit();
-          }
-      }
+  @BeforeClass
+  public static void setUp() {
+    WebDriverManager.chromedriver().setup();
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--no-sandbox");
+    options.addArguments("--disable-dev-shm-usage");
+    options.addArguments("--headless");
+    driver = new ChromeDriver(options);
+    driver.navigate().to("https://www.google.com");
+    driver.manage().window().maximize();
+    driver.manage().timeouts().implicitlyWait(120, TimeUnit.MILLISECONDS);
   }
+
+  @Test
+  public void getGoogle() {
+    System.out.println(
+        "Starting test " + new Object() {}.getClass().getEnclosingMethod().getName());
+
+    driver.get("https://www.google.com");
+    WebElement searchTxt = driver.findElement(By.name("q"));
+    WebElement submitBtn = driver.findElement(By.name("btnK"));
+
+    searchTxt.sendKeys("test");
+    submitBtn.click();
+
+    System.out.println("Current URL is: " + driver.getCurrentUrl());
+    Assert.assertTrue(driver.getTitle().contains("test"));
+    System.out.println("Current Title is: " + driver.getTitle());
+  }
+
+  @AfterClass
+  public static void tearDown() {
+    if (driver != null) {
+      driver.quit();
+    }
+  }
+}
